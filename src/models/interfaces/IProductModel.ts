a1b99542f24add2cf537f91591ecd88c9ee40d4f
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-06-16 17:52:18 -0300

    feat: Implementing the IProductModel interface

--- src/models/interfaces/IProductModel.ts
+++ src/models/interfaces/IProductModel.ts
@@ -1,11 +1,25 @@
-import { CategoryDTO } from "./ICategoryModel";
-import { ImageDTO } from "./IImageModel";
+import { Prisma } from "@prisma/client";
 
 export type ProductDTO = {
   id?: string;
   name: string;
   description: string;
-  price: number;
-  category: CategoryDTO;
-  Images?: ImageDTO[];
+  price: number | Prisma.Decimal;
+  category: {
+    id: string,
+    description: string,
+  },
+  Images?: {
+    id: string,
+    url: string,
+  }[];
 };
+
+export interface IProductModel {
+  getAll(): Promise<ProductDTO[]>;
+  getById(id: string): Promise<ProductDTO>;
+  getByName(name: string): Promise<ProductDTO[]>;
+  create(product: ProductDTO): Promise<ProductDTO>;
+  update(id: string, product: ProductDTO): Promise<ProductDTO>;
+  delete(id: string): Promise<void>;
+}
